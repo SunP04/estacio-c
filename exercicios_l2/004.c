c0b37461ab414e80e767841543b8c678dda9e912
Pedro Borges <83559896+SunP04@users.noreply.github.com>
2025-04-22 23:24:48 +0000

    fix: exercicios_l2 004

--- exercicios_l2/004.c
+++ exercicios_l2/004.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 
 int main(void) {
-    int tot;
+    int tot, idade;
+    int adolecente = 0,
+        jovem = 0,
+        adulto = 0,
+        idoso = 0;
 
     printf("Digite a quantidade total de alunos: ");
     scanf("%d", &tot);
 
-    // TODO: Action
+    for (int i = 0; i < tot; i++) {
+        do {
+            printf("Digite a idade do %dº aluno: ", i+1);
+            scanf("%d", idade);
+            if (idade < 0) printf("Idade inválida.\n");
+        } while (idade < 0);
+
+        if (idade < 18) adolecente++;
+        else if (idade < 30) jovem++;
+        else if (idade < 45) adulto++;
+        else idoso++;
+    }
+
+    printf("Diagrama da turma:\n");
+    printf("Menores de 18 anos: %d", adolecente);
+    printf("Entre 18 a 30 anos: %d", jovem);
+    printf("Entre 30 a 45 anos: %d", adulto);
+    printf("Maiores de 45 anos: %d", idoso);
 }
